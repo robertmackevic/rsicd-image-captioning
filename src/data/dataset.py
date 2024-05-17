534a382e07a234a38dc2e86298ccf3ba74f694c2
Robert Mackevič <robert@aai-labs.com>
2024-05-17 17:15:03 +0300

    Updated dataset class to filter out "bad" captions.

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -26,23 +26,33 @@ class RSICDDataset(Dataset):
         image = self.data[image_idx]["x"]
 
         all_captions = [self.tokenizer.encode(cap) for cap in self.data[image_idx]["captions"]]
+        valid_captions = [
+            cap
+            for cap in all_captions
+            if Vocab.UNK_ID not in cap and len(self.tokenizer.decode(cap)) > 1
+        ]
+
         caption_idx = index % self.num_captions_per_image
         caption = all_captions[caption_idx]
 
-        if Vocab.UNK_ID in caption:
-            # We assume that there's at least one caption that does not contain UNK tokens for an image.
-            # This is true for the RSCID dataset.
-            valid_captions = [cap for cap in all_captions if Vocab.UNK_ID not in cap]
-            caption = valid_captions[0]
-            all_captions = valid_captions + [caption] * (self.num_captions_per_image - len(valid_captions))
+        if Vocab.UNK_ID in caption or len(self.tokenizer.decode(caption)) < 2:
+            try:
+                caption = valid_captions[0]
+            # This is a case to handle "bad" captions that exists primarily in the validation set
+            # If not handled ROUGE score calculation will not work, because the captions are empty.
+            except IndexError:
+                return self.__getitem__(0)
+
+        if len(valid_captions) != self.num_captions_per_image:
+            valid_captions += [caption] * (self.num_captions_per_image - len(valid_captions))
 
         caption = LongTensor(caption)
         caption_length = LongTensor([caption.size(0)])
 
-        max_caption_length = max(len(cap) for cap in all_captions)
-        all_captions = LongTensor([
+        max_caption_length = max(len(cap) for cap in valid_captions)
+        valid_captions = LongTensor([
             cap + [Vocab.PAD_ID] * (max_caption_length - len(cap))
-            for cap in all_captions
+            for cap in valid_captions
         ])
 
-        return image, caption, caption_length, all_captions
+        return image, caption, caption_length, valid_captions
